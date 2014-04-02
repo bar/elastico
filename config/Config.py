156770ee77e94eba1c4bc6e4ba3ba30afb1fe0da
Ber Clausen <crashcookie@gmail.com>
2014-04-01 21:27:45 -0300

    pep8.

--- config/Config.py
+++ config/Config.py
@@ -21,7 +21,7 @@ class Config:
 
 	def __init__(self):
 		global args
-		args = self.parseArgs()
+		args = self.parse_args()
 		self.set()
 
 	def convert_arg_line_to_args(self, arg_line):
@@ -37,20 +37,52 @@ class Config:
 				continue
 			yield arg
 
-	def parseArgs(self):
-		parser = ArgumentParser(description = 'Index elements from MySQL databases to ElasticSearch servers.', fromfile_prefix_chars='@')
-		parser.add_argument('-v', '--verbose', action = 'count')
-		parser.add_argument('-d', '--development', action = 'store_true', help='Development environment.')
-		parser.add_argument('-c', '--db-connections', dest = 'dbConnections', type = file, help = 'File where the MySQL database connections are defined.')
-		parser.add_argument('-b', '--db-name', dest = 'dbName', help='MySQL database name.')
-		parser.add_argument('-q', '--db-queue-size', dest = 'dbQueueSize', type = int, choices = range(1, 9), help = 'MySQL databases queue size.')
-		parser.add_argument('-e', '--es-connections', dest = 'esConnections', type = file, help = 'File where the ElasticSearch server connections are defined.')
-		parser.add_argument('-n', '--es-index', dest = 'esIndex', help='ElasticSearch index.')
-		parser.add_argument('-y', '--es-type', dest = 'esType', help='ElasticSearch type.')
-		parser.add_argument('-t', '--threads', type = int, choices = range(1, 17), help = 'Number of threads to deploy.')
-		parser.add_argument('-r', '--read-buffer', type = int, dest = 'readBuffer', help = 'Size if the buffer used to read from the MySQL databases.')
-		parser.add_argument('-w', '--write-buffer', type = int, dest = 'writeBuffer', help = 'Size of the buffer used to write to the ElasticSearch servers.')
-		parser.add_argument('-l', '--limit', type = int, help = 'Limit the number of documents to index.')
+	def parse_args(self):
+		parser = ArgumentParser(
+			description = 'Index elements from MySQL databases to ElasticSearch servers.',
+			fromfile_prefix_chars = '@')
+		parser.add_argument('-v', '--verbose',
+			action = 'count')
+		parser.add_argument('-d', '--development',
+			action = 'store_true',
+			help = 'Development environment.')
+		parser.add_argument('-c', '--db-connections',
+			dest = 'db_connections',
+			type = file,
+			help = 'File where the MySQL database connections are defined.')
+		parser.add_argument('-b', '--db-name',
+			dest = 'db_name',
+			help='MySQL database name.')
+		parser.add_argument('-q', '--db-queue-size',
+			dest = 'db_queue_size',
+			type = int,
+			choices = range(1, 9),
+			help = 'MySQL databases queue size.')
+		parser.add_argument('-e', '--es-connections',
+			dest = 'es_connections',
+			type = file,
+			help = 'File where the ElasticSearch server connections are defined.')
+		parser.add_argument('-n', '--es-index',
+			dest = 'es_index',
+			help='ElasticSearch index.')
+		parser.add_argument('-y', '--es-type',
+			dest = 'es_type',
+			help = 'ElasticSearch type.')
+		parser.add_argument('-t', '--threads',
+			type = int,
+			choices = range(1, 17),
+			help = 'Number of threads to deploy.')
+		parser.add_argument('-r', '--read-buffer',
+			type = int,
+			dest = 'read_buffer',
+			help = 'Size if the buffer used to read from the MySQL databases.')
+		parser.add_argument('-w', '--write-buffer',
+			type = int,
+			dest = 'write_buffer',
+			help = 'Size of the buffer used to write to the ElasticSearch servers.')
+		parser.add_argument('-l', '--limit',
+			type = int,
+			help = 'Limit the number of documents to index.')
 		#parser.add_argument('-i', '--input', nargs='?', type = FileType('r'), default = stdin)
 		#parser.add_argument('-o', '--output', nargs='?', type = FileType('w'), default = stdout)
 		return parser.parse_args()
@@ -66,50 +98,50 @@ class Config:
 			self.verbose = args.verbose
 
 		# elasticsearch connections
-		if args.esConnections is not None:
-			self.esConnections = self.readEsConnections('es', args.esConnections)
+		if args.es_connections is not None:
+			self.es_connections = self.readEsConnections('es', args.es_connections)
 
 		# elasticsearch index name
-		if args.esIndex is not None:
-			self.esIndex = args.esIndex
+		if args.es_index is not None:
+			self.es_index = args.es_index
 		# else:
-		# 	self.esIndex = self.esIndex.format(args.prefix)
+		# 	self.es_index = self.es_index.format(args.prefix)
 
 		# elasticsearch index type
-		if args.esType is not None:
-			self.esType = args.esType
+		if args.es_type is not None:
+			self.es_type = args.es_type
 
 		# database name
-		if args.dbName is not None:
-			self.dbName = args.dbName
+		if args.db_name is not None:
+			self.db_name = args.db_name
 		# else:
-		# 	self.dbName = self.dbName.format(args.prefix)
+		# 	self.db_name = self.db_name.format(args.prefix)
 
 		# database connections
-		if args.dbConnections is not None:
-			self.dbConnections = self.readConnections('db', args.dbConnections)
+		if args.db_connections is not None:
+			self.db_connections = self.readConnections('db', args.db_connections)
 		else:
-			for i, dbConnection in enumerate(self.dbConnections):
-				self.dbConnections[i] = list(dbConnection)
+			for i, dbConnection in enumerate(self.db_connections):
+				self.db_connections[i] = list(dbConnection)
 
-		for dbConnection in self.dbConnections:
-			dbConnection.insert(0, self.dbName)
+		for dbConnection in self.db_connections:
+			dbConnection.insert(0, self.db_name)
 
 		# databases queue size
-		if args.dbQueueSize is not None:
-			self.dbQueueSize = args.dbQueueSize
+		if args.db_queue_size is not None:
+			self.db_queue_size = args.db_queue_size
 
 		# threads
 		if args.threads is not None:
 			self.threads = args.threads
 
 		# read buffer size
-		if args.readBuffer is not None:
-			self.readBuffer = args.readBuffer
+		if args.read_buffer is not None:
+			self.read_buffer = args.read_buffer
 
 		# write buffer size
-		if args.writeBuffer is not None:
-			self.writeBuffer = args.writeBuffer
+		if args.write_buffer is not None:
+			self.write_buffer = args.write_buffer
 
 		# documents limit
 		if args.limit is not None:
@@ -119,38 +151,38 @@ class Config:
 		if development:
 			config = {
 				'verbose': 3,
-				'esIndex': 'dmoz',
-				'esType': 'category',
-				'dbName': 'dmoz',
-				'dbConnections': [
-					('127.0.0.1', 'elastico', 'elastico'),
-				],
-				'dbQueueSize': 4,
-				'threads': 16,
-				'readBuffer': 100,
-				'writeBuffer': 1000,
-				'limit': 2000,
-				'esConnections': [
+				'es_connections': [
 					('http', '192.168.0.1', '9200')
 				],
+				'es_index': 'dmoz',
+				'es_type': 'category',
+				'db_connections': [
+					('127.0.0.1', 'elastico', 'elastico'),
+				],
+				'db_name': 'dmoz',
+				'db_queue_size': 4,
+				'threads': 16,
+				'read_buffer': 100,
+				'write_buffer': 1000,
+				'limit': 2000,
 			}
 		else:
 			config = {
 				'verbose': 0,
-				'esIndex': 'dmoz',
-				'esType': 'category',
-				'dbName': 'dmoz',
-				'dbConnections': [
-					('127.0.0.1', 'elastico', 'elastico'),
-				],
-				'dbQueueSize': 4,
-				'threads': 16,
-				'readBuffer': 100,
-				'writeBuffer': 1000,
-				'limit': None,
-				'esConnections': [
+				'es_connections': [
 					('http', '127.0.0.1', '9200')
 				],
+				'es_index': 'dmoz',
+				'es_type': 'category',
+				'db_connections': [
+					('127.0.0.1', 'elastico', 'elastico'),
+				],
+				'db_name': 'dmoz',
+				'db_queue_size': 4,
+				'threads': 16,
+				'read_buffer': 100,
+				'write_buffer': 1000,
+				'limit': None,
 			}
 
 		return config
@@ -158,26 +190,25 @@ class Config:
 	def readConnections(self, type, file):
 		"""Read connections configuration from file"""
 		if type == 'es':
-			getConnection = self.getEsConnection
+			connection = self.es_connection
 		else:
-			getConnection = self.getDbConnection
+			connection = self.db_connection
 
 		list = []
 		for line in file:
 			try:
-				connection = getConnection(line.split())
-				list.append(connection)
+				list.append(connection(line.split()))
 			except ValueError:
 				continue
 		if list is not []:
 			return list
 
-	def getEsConnection(self, line):
+	def es_connection(self, line):
 		"""Read connection configuration for Elasticsearch"""
 		connection = (hostname, username, password) = line
 		return connection
 
-	def getDbConnection(self, line):
+	def db_connection(self, line):
 		"""Read connection configuration for MySQL"""
 		connection = (schema, hostname, port) = line
 		return connection
