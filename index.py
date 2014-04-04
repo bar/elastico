248cad5afeb47eab7b430630dae76ff12fce1511
Ber Clausen <crashcookie@gmail.com>
2014-04-04 18:24:57 -0300

    Update with the latest fixes.

--- index.py
+++ index.py
@@ -27,102 +27,74 @@ from utils.errors import BadConfigError
 # Config
 from config.Config import Config
 
-# Soup
-from model.Soup import Soup
+# Db connector
+from connectors.SQLSoup import Connector as DbConnector
 
 # Indexer
 from indexer.Indexer import Indexer
 
 # Thread
-# from utils.Thread import Thread
+from utils.Thread import Thread
 
 
 def main(script, *args, **kwargs):
-	startTime = time.time()
+	start_time = time.time()
 
-	IndexerConfig = Config()
+	# Config options
+	config = Config()
 
-	threadList = range(IndexerConfig.threads)
+	# Threads list
 	threads = []
-	DbQueue = Queue()
 
-	db_connections = IndexerConfig.db_connections
+	# Db connections queue
+	db_connections_queue = Queue()
 
-	for _ in range(IndexerConfig.db_queue_size):
+	# Db connections list
+	db_connections = config.db_connections
+
+	# temporary MySQL settings
+	source_table = 'categories'
+
+	# Populte db connections queue (round robin)
+	for _ in range(config.db_queue_size):
 		db_connection = db_connections.pop(0)
-		IndexerSoup = Soup(db_connection)
-		DbQueue.put(IndexerSoup.build())
+		db_connections_queue.put(DbConnector(db_connection).db())
 		db_connections.append(db_connection)
 
-	vprint('Index: {:s}'.format(IndexerConfig.es_index))
-	vprint('Type: {:s}'.format(IndexerConfig.es_type))
-	vprint('Threads: {:d}'.format(IndexerConfig.threads))
-	vprint('DB Queue size: {:d}'.format(IndexerConfig.db_queue_size))
-	vprint('Read buffer: {:d}'.format(IndexerConfig.read_buffer))
-	vprint('Write buffer: {:d}'.format(IndexerConfig.write_buffer))
+	vprint('Index: {:s}'.format(config.es_index))
+	vprint('Type: {:s}'.format(config.es_type))
+	vprint('Threads: {:d}'.format(config.threads))
+	vprint('DB Queue size: {:d}'.format(config.db_queue_size))
+	vprint('Read buffer: {:d}'.format(config.read_buffer))
+	vprint('Write buffer: {:d}'.format(config.write_buffer))
 
-	# import ipdb; ipdb.set_trace()
-	EsIndexer = Indexer(Soup(db_connections.pop(0)).build(), IndexerConfig.limit)
+	# TODO
+	IndexerConnector = DbConnector(db_connections[0]).db()
+	source_relationships = False
 
-# 	# Connection to ElasticSearch (write)
-# 	# retry_time = 10
-# 	# timeout = 10
-# 	EsServer = ES(server = IndexerConfig.elasticConnections, bulk_size = IndexerConfig.write_buffer)
-# 	EsServer.indices.create_index_if_missing(IndexerConfig.es_index)
+	PrimaryModel =  IndexerConnector.build(source_table, source_relationships)
 
-# 	# Create new threads
-# 	for i in threadList:
-		# thread = Thread.Thread(EsIndexer.consume, (
-		# 	startTime,
-		# 	str(i+1),
-		# 	DbQueue,
-		# 	EsServer,
-		# 	IndexerConfig.es_index,
-		# 	IndexerConfig.es_type,
-		# 	IndexerConfig.read_buffer,
-		# 	IndexerConfig.write_buffer))
-# 		threads.append(thread)
+	if not PrimaryModel:
+		raise ConnectorError
 
-# 	for t in threads:
-# 		t.start()
+	indexer = Indexer(PrimaryModel, limit=config.limit)
 
-# 	for t in threads:
-# 		t.join()
+	es_server = ES(server=config.es_connections, bulk_size=config.write_buffer)
 
-# 	vprint('Refreshing index...')
-# 	EsServer.refresh()
+	indexer.consume(
+		start_time,
+		str(1),
+		db_connections_queue,
+		es_server,
+		config.es_index,
+		config.es_type,
+		config.read_buffer,
+		config.write_buffer)
 
-# 	vprint('Elapsed: {:f}'.format(time.time() - startTime))
+	vprint('Refreshing index...')
+	# es_server.refresh()
+
+	vprint('Elapsed: {:f}'.format(time.time() - start_time))
 
 if __name__ == '__main__':
 	main(*sys.argv)
-
-
-# def main(script, flag='with'):
-#     """This example runs two threads that print a sequence, sleeping
-#     one second between each.  If you run it with no command-line args,
-#     or with the argument 'with', you should be able it interrupt it
-#     with Control-C.
-
-#     If you run it with the command-line argument 'without', and press
-#     Control-C, you will probably get a traceback from the main thread,
-#     but the child thread will run to completion, and then print a
-#     traceback, no matter how many times you try to interrupt.
-#     """
-
-#     if flag == 'with':
-#         Watcher()
-#     elif flag != 'without':
-#         print 'unrecognized flag: ' + flag
-#         sys.exit()
-
-#     t = range(1, 10)
-
-#     # create a child thread that runs counter
-#     ThreadWatcher.MyThread(counter, t)
-
-#     # run counter in the parent thread
-#     ThreadWatcher.counter(t)
-
-# if __name__ == '__main__':
-#     main(*sys.argv)
