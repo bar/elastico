24d83759cf8279b936d6890173e55e0808547ff1
Ber Clausen <crashcookie@gmail.com>
2014-04-04 18:59:45 -0300

    Remove stupid functions and argument initializations and default values.

--- indexer/BaseIndexer.py
+++ indexer/BaseIndexer.py
@@ -23,21 +23,23 @@ class BaseIndexer(object):
 
 	__metaclass__ = ABCMeta
 
-	def __init__(self, Model, limit=None):
-		self.count = 0
-		self.empty = False
+	buffer = None
+	count = 0
+	empty = False
+	lock = Lock()
+	total = 0
+	read_buffer_size = 10
+	write_buffer_size = 1000
 
+	def __init__(self, Model, limit=None):
 		# Producer
 		self.produce(Model, limit)
-
-		# Lock
-		self.lock = Lock()
 
 		# Manage threads
 		ThreadWatcher.ThreadWatcher()
 
 	@abstractmethod
-	def produce(self, Model, limit=None):
+	def produce(self, Model, limit):
 		"""Fills the buffer with a generator based on MySQL ids that will be later used as the entry point
 		to construct each objects.
 
@@ -54,7 +56,15 @@ class BaseIndexer(object):
 		raise NotImplementedError()
 
 	@abstractmethod
-	def consume(self, start_time, thread_name, db_model_queue, es_server, es_index, es_type, read_buffer_size=10, write_buffer_size=1000):
+	def consume(self,
+		start_time,
+		thread_name,
+		db_model_queue,
+		es_server,
+		es_index,
+		es_type,
+		read_buffer_size,
+		write_buffer_size):
 		"""Retrieves data from models an its associations.
 
 		It handles the creation of Elasticsearch documents, and the index process.
@@ -70,155 +80,3 @@ class BaseIndexer(object):
 			write_buffer_size (integer): Number of objects to accumulate in each index chunk.
 		"""
 		raise NotImplementedError()
-
-	@abstractmethod
-	def buildModel(self, Model):
-		"""One liner description
-
-		Args:
-			param1 (type): description
-
-		Returns:
-			type: description
-		"""
-		raise NotImplementedError()
-
-	# @abstractmethod
-	# def fetch_condition(self, site, filter):
-	# 	raise NotImplementedError()
-
-	def getMappedObject(self, Mapped, relation):
-		""" Gets a Mapped object based on an existing relation.
-
-		:type Mapped: object
-		:param Mapped: Mapped object from where we get its relation.
-
-		:type relation: string
-		:param relation: String with the model to relate to.
-
-		:rtype: mixed
-		:return: Mapped object if exists, None otherwise.
-		"""
-
-		return getattr(Mapped, relation)
-
-	def belongsTo(self, Mapped, relation):
-		""" Gets a Mapped object based on an existing belongsTo relation.
-
-		:type Mapped: object
-		:param Mapped: Mapped object from where we get its relation.
-
-		:type relation: string
-		:param relation: String with the model to relate to.
-
-		:rtype: mixed
-		:return: Mapped object if exists, None otherwise.
-		"""
-
-		return self.getMappedObject(Mapped, relation)
-
-	def hasMany(self, Mapped, relation):
-		""" Gets a Mapped object based on an existing hasMany relation.
-
-		:type Mapped: object
-		:param Mapped: Mapped object from where we get its relation.
-
-		:type relation: string
-		:param relation: String with the model to relate to.
-
-		:rtype: mixed
-		:return: Mapped object if exists, None otherwise.
-		"""
-		return self.getMappedObject(Mapped, relation)
-
-	def hasAndBelongsToMany(self, Mapped, relation):
-		""" Gets a Mapped object based on an existing hasAndBelongsToMany relation.
-
-		:type Mapped: object
-		:param Mapped: Mapped object from where we get its relation.
-
-		:type relation: string
-		:param relation: String with the model to relate to.
-
-		:rtype: mixed
-		:return: Mapped object if exists, None otherwise.
-		"""
-		return self.getMappedObject(Mapped, relation)
-
-	def getEmptySchema(self, table):
-		""" Get empty structure of a table.
-
-		:type table: object
-		:param table: Mapped object table.
-
-		:rtype: dict
-		:return: A dict with column names with None as values.
-		"""
-		try:
-			schema = {}
-			for c in table.c.keys():
-				schema[c] = None
-
-			return schema
-
-		except NoSuchTableError, e:
-			print('There is no such a table called {}').format(e.args[0])
-			exit(1)
-
-	def _mappedToModel(self, Mapped):
-		""" Map an SqlSoup object to a CakePHP model.
-
-		:type Mapped: object
-		:param Mapped: Mapped object to convert.
-
-		:rtype: dict
-		:return: Mapped values converted to dict.
-		"""
-
-		if not isinstance(type(Mapped), TableClassType):
-			return None
-
-		schema = self.getEmptySchema(Mapped._table)
-		mappedObjectDict = Mapped.__dict__
-
-		data = {}
-		for k in schema:
-			data[k] = mappedObjectDict[k]
-
-		return data
-
-	def mappedToModel(self, Mapped):
-		""" Convert a Mapped object into a dict.
-
-		If Mapped has other maps inside, also converts them too.
-
-		:type Mapped: object
-		:param Mapped: Mapped object to convert.
-
-		:rtype: list
-		:return: A list with dicts of converted mapped objects.
-		"""
-		if isinstance(Mapped, list):
-			Model = []
-			for v in Mapped:
-				_v = self._mappedToModel(v)
-				if _v != None:
-					Model.append(_v)
-			return Model
-		else:
-			return self._mappedToModel(Mapped)
-
-	def translateModel(self, Mapped):
-		""" Converts to dict all members of dict of mapped objects.
-
-		:type Mapped: dict
-		:param Mapped: Dict with mapped object as its values.
-
-		:rtype: dict
-		:return: Dict with mapped values converted.
-		"""
-		Model = {}
-		for key in Mapped:
-			Model[key] = self.mappedToModel(Mapped[key])
-
-		return Model
